afad73f2faa0659931cbb7dc7f6a6af6a37d36b0
Angie Leiva <38085631+ancarolm@users.noreply.github.com>
2021-05-19 01:03:57 -0600

    add files

--- demo/src/main/java/testing/demo/ReadObject.java
+++ demo/src/main/java/testing/demo/ReadObject.java
@@ -6,13 +6,21 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+
+import testing.readexcel.ExcelFile;
+
 public class ReadObject {
 	
 	Properties p = new Properties();
 	 
 	public Properties getObjectRepository() throws IOException{
 
-		InputStream stream = new FileInputStream("../demo/src/main/java/config/config.properties");
+
+		//InputStream stream = new FileInputStream("../demo/src/main/java/config/config.properties");
+		
+		InputStream stream = new FileInputStream("../demo/src/main/resources/config.xlsx");
+		
 		
 		p.load(stream);
 		
